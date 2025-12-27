aa0749ce0cd32abb350a77cea2853f7d61a53c93
ashishkumartiwari-dot <ashish.kumar.tiwari@rungta.org>
2025-12-27 09:59:50 +0530

    Create practical8.3.c

---
+++ practical8.3.c
@@ -0,0 +1,24 @@
+// Name - Ashish kumar tiwari- ERP - 10309
+#include<stdio.h>
+//Practical 8:Q.3. write a function to print a right angle triagle of stars of height n.//
+int star(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    star(n);
+    return 0;
+}
+
+/* Output - 5
+*
+**
+***
+****
+***** */
