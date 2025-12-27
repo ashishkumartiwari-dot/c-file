603d9e5880037213c17405dee1cea18d595876e8
ashishkumartiwari-dot <ashish.kumar.tiwari@rungta.org>
2025-12-27 09:55:36 +0530

    Create practical7.3.c

---
+++ practical7.3.c
@@ -0,0 +1,24 @@
+// Name - Ashish kumar tiwari  - ERP - 10309
+#include<stdio.h>
+int main(){
+    //Practical 7: Q.3.pattern//
+    int n;
+    scanf("%d",&n);
+    char ch='A';
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            printf("%c",ch);
+            ch++;
+        }
+        printf("\n");
+        ch='A';
+    }
+    return 0;
+}
+
+/* Output - 5
+A
+AB
+ABC
+ABCD
+ABCDE */
