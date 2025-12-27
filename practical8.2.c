ab49a1e76b1e3c790ae9b30359ddb3fb3e49754e
ashishkumartiwari-dot <ashish.kumar.tiwari@rungta.org>
2025-12-27 09:58:32 +0530

    Create practical8.2.c

---
+++ practical8.2.c
@@ -0,0 +1,16 @@
+// Name - Ashish kumar tiwari - ERP - 10309
+#include<stdio.h>
+//Practical 8:Q.2. Swap 2 number using pass by reference//
+void swap(int *a,int *b){
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
+int main(){
+    int a=5,b=3;
+    swap(&a,&b);
+    printf("after swapping a=%d and b=%d",a,b);
+    return 0;
+}
+
+/* Output - after swapping a=3 and b=5 */
